00a272bd5fe54a3d2505b9da9f820a0864f5fce1
Nikhil Krishnan <75375614+krisboyy@users.noreply.github.com>
2023-10-07 11:37:34 +0530

    Added code and input file for experiment 1

---
+++ Exp1/input.c
@@ -0,0 +1,16 @@
+/* This is a  *
+* multiline comment */
+main()
+{
+int _a78=5,b=10,c_b;  //This is a single line comment
+    _c8 = b/a;
+	if(a>b)
+		big=a;
+while(a>=b&&c<9)
+{
+  a*=2&c;
+}
+a/=b;
+printf("Hello World");
+}
+
